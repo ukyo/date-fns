28be1fbe348e4e04c67785bfcacc4d4042fe5e3f
ukyo <ukyo.web@gmail.com>
2016-12-26 19:54:41 +0900

    Prepare release v1.21.3

--- scripts/release_npm.sh
+++ scripts/release_npm.sh
@@ -27,7 +27,7 @@ done
 cp dist/date_fns_docs.json $dir/docs.json
 find "$dir" -type f -name "test.js" -delete
 find "$dir" -type f -name "benchmark.js" -delete
-cd "$dir" || exit
-npm publish --access=public
-cd - || exit
-rm -rf "$dir"
+# cd "$dir" || exit
+# npm publish --access=public
+# cd - || exit
+# rm -rf "$dir"
